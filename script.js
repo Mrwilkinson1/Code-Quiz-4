70aeceaeeb9b757c52c6624d9ee489c09ddacd40
Alex Wilkinson <mrwilkinson1@gmail.com>
2022-07-10 14:38:40 -0400

    questions answers

---
+++ script.js
@@ -0,0 +1,34 @@
+var sportsInfo = [{
+        question: "With which team did Michael Jordan win 6 titles?"
+        a: "Atlanta Hawks",
+        b: "New York Knicks", 
+        c: "Golden State Warriors", 
+        d: "Chicago Bulls",
+        correct "d",
+}  
+{
+question: "How many carrer homeruns did Hank Aaron have?"
+        a: "755",
+        b: "801", 
+        c: "678", 
+        d: "404",
+        correct "a",
+},
+{
+question: "What number did Tom Brady wear in 2003-04?"
+        a: "7",
+        b: "12",
+        c: "84",
+        d: "24",
+        correct "b",
+}
+{
+question: "What is Tiger's full name?"
+        a: "Bill Tiger Woods",
+        b: "Elroy Cat Woods",
+        c: "Tony Tiger Woods",
+        d: "Eldrik Tont Woods",
+        correct "d",
+},
+];
+
